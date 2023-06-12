8b3ec5532012853f9b008237b5f7da49f20abd27
André Macías <andremacias@lasallistas.org.mx>
2023-06-11 20:46:01 -0600

    Define service layer

--- src/main/java/com/andre/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/andre/cruddemo/rest/EmployeeRestController.java
@@ -2,6 +2,8 @@ package com.andre.cruddemo.rest;
 
 import com.andre.cruddemo.dao.EmployeeDAO;
 import com.andre.cruddemo.entity.Employee;
+import com.andre.cruddemo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +13,15 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class EmployeeRestController {
-    private EmployeeDAO employeeDAO;
     //inject employee dao with constructor injection
-    public EmployeeRestController(EmployeeDAO theEmployeeDAO){
-        employeeDAO = theEmployeeDAO;
+    private EmployeeService employeeService;
+    @Autowired
+    public EmployeeRestController(EmployeeService theEmployeeService){
+        employeeService = theEmployeeService;
     }
     //expose "/employees" and return a list of employees
     @GetMapping("/employees")
     public List<Employee> findAll(){
-        return employeeDAO.findAll();
+        return employeeService.findAll();
     }
 }
---
+++ src/main/java/com/andre/cruddemo/service/EmployeeService.java
@@ -0,0 +1,10 @@
+package com.andre.cruddemo.service;
+
+import com.andre.cruddemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> findAll();
+}
