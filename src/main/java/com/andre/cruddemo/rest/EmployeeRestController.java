b23f9e5936a9f43944d4ed0bf3f026153c41f8bc
André Macías <andremacias@lasallistas.org.mx>
2023-06-11 21:04:46 -0600

    Get Single Employee

--- src/main/java/com/andre/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/andre/cruddemo/rest/EmployeeRestController.java
@@ -4,7 +4,9 @@ import com.andre.cruddemo.dao.EmployeeDAO;
 import com.andre.cruddemo.entity.Employee;
 import com.andre.cruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +25,15 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> findAll(){
         return employeeService.findAll();
+    }
+
+    //Add mapping for GET /employees/{employeeId}
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId){
+        Employee theEmployee = employeeService.findById(employeeId);
+        if(theEmployee == null){
+            throw new RuntimeException("Employee id not found - "+employeeId);
+        }
+        return theEmployee;
     }
 }
