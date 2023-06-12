a990d852d610e8de536df97cf88695bd9232b4e6
André Macías <andremacias@lasallistas.org.mx>
2023-06-11 20:51:50 -0600

    Add, Update and Delete Coding

--- src/main/java/com/andre/cruddemo/dao/EmployeeDAOImpl.java
+++ src/main/java/com/andre/cruddemo/dao/EmployeeDAOImpl.java
@@ -24,4 +24,28 @@ public class EmployeeDAOImpl implements EmployeeDAO {
         //return the results
         return employees;
     }
+
+    @Override
+    public Employee findById(int theId) {
+        //get employee
+        Employee theEmployee = entityManager.find(Employee.class,theId);
+        //return the employee
+        return theEmployee;
+    }
+
+    @Override
+    public Employee save(Employee theEmployee) {
+        //save employee
+        Employee dbEmployee = entityManager.merge(theEmployee);//si el id !=0 entonces lo guarda, en caso contrario inserta un nuevo registro
+        //return the dbEmployee
+        return dbEmployee;
+    }
+
+    @Override
+    public void deleteById(int theId) {
+        //find employee by id
+        Employee theEmployee = entityManager.find(Employee.class,theId);
+        //remove employee
+        entityManager.remove(theEmployee);
+    }
 }
