25f63bb7c8e19f0d23ce99fddce3c28ad749bd69
André Macías <andremacias@lasallistas.org.mx>
2023-06-19 16:37:16 -0600

    Changing REST resource path

--- src/main/java/com/andre/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/andre/cruddemo/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.andre.cruddemo.dao;
 
 import com.andre.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
     //thats it, no need to write any code
 }
